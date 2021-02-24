5307c2d850b8e91b93107a068acf63a635bf43b8
Yenowme <76904042+Yenowme@users.noreply.github.com>
2021-02-24 10:41:38 +0900

    ex07

--- c00/main.c
+++ c00/main.c
@@ -6,6 +6,8 @@ void ft_print_numbers(void);
 void ft_is_negative(int n);
 void ft_print_comb(void);
 void ft_print_comb2(void);
+void ft_putnbr(int nb);
+//void ft_print_combn(int n);
 
 int	main(void)
 {
@@ -23,5 +25,17 @@ int	main(void)
 	ft_print_comb();
 	printf("\n=====6=======\n");
 	ft_print_comb2();
+	printf("\n=====7=======\n");
+	ft_putnbr(-2147483648);
+	printf("\n");
+	ft_putnbr(2147483647);
+	printf("\n");
+	ft_putnbr(0);
+	printf("\n");
+	ft_putnbr(123);
+	printf("\n");
+	printf("\n=====7=======\n");
+//	ft_print_combn(1);
+//	ft_print_combn(10);
 	return 0;
 }
