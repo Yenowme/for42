11b9f184ea761ee2878fe187bd30328e59bbb141
Yenowme <76904042+Yenowme@users.noreply.github.com>
2021-03-06 10:44:53 +0900

    c08 start

--- c08/ex00/ft.h
+++ c08/ex00/ft.h
@@ -1,5 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yejeong <yejeong@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/05 18:35:04 by yejeong           #+#    #+#             */
+/*   Updated: 2021/03/05 18:51:06 by yejeong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
 
 void	ft_swap(int *a, int *b);
 void	ft_putstr(char *str);
 int		ft_strlen(char *str);
 int		ft_strcmp(char *s1, char *s2);
+
+#endif
